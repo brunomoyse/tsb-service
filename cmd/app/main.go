d585ace66bdbaf221092a900134ba9ad38e1f56c
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2025-03-01 19:54:09 +0100

    Fix token generation

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -112,7 +112,7 @@ func main() {
 	api.GET("/oauth/google", userHandler.GoogleAuthHandler)
 	api.GET("/oauth/google/callback", userHandler.GoogleAuthCallbackHandler)
 
-	api.POST("tokens:refresh", userHandler.RefreshTokenHandler)
+	api.POST("/tokens:refresh", userHandler.RefreshTokenHandler)
 
 	// Create a subgroup for routes that require authentication.
 	authGroup := api.Group("/me")
