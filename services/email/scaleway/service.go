d9b9cd46184b68f5b66e497970496d7fb9f44607
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2025-04-13 18:25:03 +0200

    Split service and html render files

--- services/email/scaleway/service.go
+++ services/email/scaleway/service.go
@@ -1,36 +1,20 @@
 package scaleway
 
 import (
-	"bytes"
-	"embed"
 	"fmt"
 	"github.com/scaleway/scaleway-sdk-go/logger"
-	"github.com/shopspring/decimal"
-	"html/template"
 	"os"
 	orderDomain "tsb-service/internal/modules/order/domain"
-	"tsb-service/pkg/utils"
 
 	temv1alpha1 "github.com/scaleway/scaleway-sdk-go/api/tem/v1alpha1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 	userDomain "tsb-service/internal/modules/user/domain"
 )
 
-//go:embed templates/*/*.html
-var emailFS embed.FS
-
-// UserTest is a placeholder struct for test email data.
-type UserTest struct {
-	FirstName string
-	LastName  string
-	Email     string
-	Language  string
-}
-
 // temClient is our instance for interacting with Scaleway TEM.
 var temClient *temv1alpha1.API
 
-var req *temv1alpha1.CreateEmailRequest
+var baseReq *temv1alpha1.CreateEmailRequest
 
 // InitService initializes the Scaleway TEM client using credentials from environment variables.
 func InitService() error {
@@ -59,7 +43,7 @@ func InitService() error {
 	senderName := os.Getenv("SCW_SENDER_NAME")
 
 	// Load infos in the base CreateEmailRequest
-	req = &temv1alpha1.CreateEmailRequest{
+	baseReq = &temv1alpha1.CreateEmailRequest{
 		Region: scw.Region(region),
 		From: &temv1alpha1.CreateEmailRequestAddress{
 			Email: senderEmail,
@@ -71,113 +55,9 @@ func InitService() error {
 	return nil
 }
 
-// loadTemplate loads the template file from the embedded file system using the provided path.
-func loadTemplate(path string) (*template.Template, error) {
-	tmpl, err := template.ParseFS(emailFS, path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse template %q: %w", path, err)
-	}
-	return tmpl, nil
-}
-
-func renderVerifyEmail(path string, user userDomain.User, verifyLink string) (string, error) {
-	tmpl, err := loadTemplate(path)
-
-	if err != nil {
-		return "", err
-	}
-
-	data := struct {
-		UserName   string
-		VerifyLink string
-	}{
-		UserName:   fmt.Sprintf("%s %s", user.FirstName, user.LastName),
-		VerifyLink: verifyLink,
-	}
-
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, data); err != nil {
-		return "", fmt.Errorf("failed to execute verify template: %w", err)
-	}
-	return buf.String(), nil
-}
-
-func renderWelcomeEmail(path string, user userDomain.User, menuLink string) (string, error) {
-	tmpl, err := loadTemplate(path)
-	if err != nil {
-		return "", err
-	}
-
-	data := struct {
-		UserName string
-		MenuLink string
-	}{
-		UserName: fmt.Sprintf("%s %s", user.FirstName, user.LastName),
-		MenuLink: menuLink,
-	}
-
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, data); err != nil {
-		return "", fmt.Errorf("failed to execute welcome template: %w", err)
-	}
-	return buf.String(), nil
-}
-
-func renderOrderPendingEmail(path string, u userDomain.User, op []orderDomain.OrderProduct, o orderDomain.Order) (string, error) {
-	tmpl, err := loadTemplate(path)
-	if err != nil {
-		return "", err
-	}
-
-	type OrderProductView struct {
-		Name       string
-		Quantity   int64
-		TotalPrice string
-	}
-
-	var orderViews []OrderProductView
-	for _, item := range op {
-		orderViews = append(orderViews, OrderProductView{
-			Name:       fmt.Sprintf("%s - %s", item.Product.CategoryName, item.Product.Name),
-			Quantity:   item.Quantity,
-			TotalPrice: utils.FormatDecimal(item.TotalPrice),
-		})
-	}
-
-	subtotal := decimal.NewFromInt(0)
-
-	for _, item := range op {
-		subtotal = subtotal.Add(item.TotalPrice)
-	}
-
-	data := struct {
-		UserName         string
-		OrderItems       []OrderProductView
-		OrderType        string
-		SubtotalPrice    string
-		TakeawayDiscount string
-		DeliveryFee      string
-		TotalPrice       string
-	}{
-		UserName:         fmt.Sprintf("%s %s", u.FirstName, u.LastName),
-		OrderItems:       orderViews,
-		OrderType:        string(o.OrderType),
-		SubtotalPrice:    utils.FormatDecimal(subtotal),
-		TakeawayDiscount: utils.FormatDecimal(decimal.NewFromFloat(0)), // @TODO
-		DeliveryFee:      utils.FormatDecimal(*o.DeliveryFee),
-		TotalPrice:       utils.FormatDecimal(o.TotalPrice),
-	}
-
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, data); err != nil {
-		return "", fmt.Errorf("failed to execute order pending template: %w", err)
-	}
-	return buf.String(), nil
-}
-
 func SendVerificationEmail(user userDomain.User, lang string, verificationURL string) error {
-	// Copy req to avoid modifying the original request.
-	newReq := *req
+	// Copy baseReq to avoid modifying the original request.
+	newReq := *baseReq
 
 	userFullName := fmt.Sprintf("%s %s", user.FirstName, user.LastName)
 
@@ -228,8 +108,8 @@ func SendVerificationEmail(user userDomain.User, lang string, verificationURL st
 }
 
 func SendWelcomeEmail(user userDomain.User, lang, menuURL string) error {
-	// Copy req to avoid modifying the original request.
-	newReq := *req
+	// Copy baseReq to avoid modifying the original request.
+	newReq := *baseReq
 
 	userFullName := fmt.Sprintf("%s %s", user.FirstName, user.LastName)
 
@@ -279,8 +159,8 @@ func SendWelcomeEmail(user userDomain.User, lang, menuURL string) error {
 }
 
 func SendOrderPendingEmail(user userDomain.User, lang string, order orderDomain.Order, op []orderDomain.OrderProduct) error {
-	// Copy req to avoid modifying the original request.
-	newReq := *req
+	// Copy baseReq to avoid modifying the original request.
+	newReq := *baseReq
 
 	userFullName := fmt.Sprintf("%s %s", user.FirstName, user.LastName)
 
