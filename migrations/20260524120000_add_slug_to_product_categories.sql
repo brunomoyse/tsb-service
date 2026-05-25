408b2c67cd5786b8a9547cafcad16a22c8e4d7e6
Bruno Moyse <bruno.moyse@nccn.fgov.be>
2026-05-25 18:21:50 +0200

    feat(product): add slug for menu-lunch and ramen categories

--- migrations/20260524120000_add_slug_to_product_categories.sql
+++ migrations/20260524120000_add_slug_to_product_categories.sql
@@ -3,6 +3,7 @@ ALTER TABLE product_categories ADD COLUMN slug text;
 
 UPDATE product_categories SET slug = 'menu-plateau'    WHERE id = '9e1992c3-2e26-4082-8219-06ca8da5d66f';
 UPDATE product_categories SET slug = 'menu-bento-box'  WHERE id = '9e1992c3-b51e-4b9d-845c-ec88b640977a';
+UPDATE product_categories SET slug = 'menu-lunch'      WHERE id = 'b72ff149-6d3a-45a2-9e52-f824e2c80727';
 UPDATE product_categories SET slug = 'sushi'           WHERE id = '9e1992c4-37c7-493e-b69a-0b71cc7af146';
 UPDATE product_categories SET slug = 'maki'            WHERE id = '9e1992c4-be65-4adf-8e18-6035219974ee';
 UPDATE product_categories SET slug = 'gunkan'          WHERE id = '9e1992c5-4002-4797-ba9d-feb545add16e';
@@ -14,6 +15,7 @@ UPDATE product_categories SET slug = 'special-roll'    WHERE id = '9e1992c7-c630
 UPDATE product_categories SET slug = 'chirashi'        WHERE id = '9e1992c8-49d8-4bd1-8811-4ce3978789b7';
 UPDATE product_categories SET slug = 'sashimi'         WHERE id = '9e1992c8-c8fd-4974-803d-cd7f9c0c1e6d';
 UPDATE product_categories SET slug = 'poke-bowl'       WHERE id = '9e1992c9-4c21-4723-a416-96550589eb7c';
+UPDATE product_categories SET slug = 'ramen'           WHERE id = 'a2de2102-7930-4109-b162-ebb03585f6ac';
 UPDATE product_categories SET slug = 'tokyo-hot'       WHERE id = '9e1992c9-d637-4dcc-ae10-bd9a445495b8';
 UPDATE product_categories SET slug = 'accompagnement'  WHERE id = '9e1992ca-e360-4498-ab6a-62852f8f213b';
 UPDATE product_categories SET slug = 'boisson'         WHERE id = '9e1992cb-66d5-4334-b6e6-669f37047773';
